6965a8f0fd24e0412cbaa1a4d136d235a6e4778f
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-14 17:57:38 +0100

    Adds flags (v-if v-if-else)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,21 +5,55 @@
       <li v-for="film in films" :key="film.id">
         <h5>Titolo: {{ film.title }}</h5>
         <h6>Titolo originale: {{ film.original_title }}</h6>
-        <p>
-          Lingua: {{ film.original_language }}
-          <br />
-          Voto: {{ film.vote_average }}
-        </p>
+        <div>
+          Lingua originale:
+          <span v-if="film.original_language === 'en'"
+            ><img class="flag" src="../assets/img/en.png" alt="English Flag"
+          /></span>
+          <span v-else-if="film.original_language === 'it'"
+            ><img
+              class="flag"
+              src="../assets/img/it.png"
+              alt="Bandiera Italiana"
+          /></span>
+          <span v-else-if="film.original_language === 'de'"
+            ><img
+              class="flag"
+              src="../assets/img/de.png"
+              alt="Bandiera Tedesca"
+          /></span>
+          <span v-else-if="film.original_language === 'fr'"
+            ><img
+              class="flag"
+              src="../assets/img/fr.png"
+              alt="Bandiera Francese"
+          /></span>
+          <span v-else
+            ><strong>{{ film.original_language }}</strong></span
+          >
+        </div>
+        <div>Voto: {{ film.vote_average }}</div>
       </li>
       <h3>Serie TV</h3>
       <li v-for="serie in tvSeries" :key="serie.id">
         <h5>Titolo: {{ serie.name }}</h5>
         <h6>Titolo originale: {{ serie.original_name }}</h6>
-        <p>
-          Lingua: {{ serie.original_language }}
-          <br />
-          Voto: {{ serie.vote_average }}
-        </p>
+        <div>
+          Lingua originale:
+          <span v-if="serie.original_language === 'en'"
+            ><img class="flag" src="../assets/img/en.png" alt="English Flag"
+          /></span>
+          <span v-else-if="serie.original_language === 'it'"
+            ><img
+              class="flag"
+              src="../assets/img/it.png"
+              alt="Bandiera Italiana"
+          /></span>
+          <span v-else
+            ><strong>{{ serie.original_language }}</strong></span
+          >
+        </div>
+        <div>Voto: {{ serie.vote_average }}</div>
       </li>
     </ul>
     <h2 v-else class="ms-2">Cerca un film o una serie tv</h2>
@@ -34,4 +68,8 @@ export default {
 </script>
 
 <style>
+.flag {
+  width: 35px;
+  height: auto;
+}
 </style>
