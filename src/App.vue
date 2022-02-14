3f8dedd5e4846ef1f320617f49f732c2053f6b2b
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-14 13:53:49 +0100

    Creates Header.vue + Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
-  <div id="app">
+  <div id="boolfix">
+    <Header />
+    <Main />
   </div>
 </template>
 
 <script>
-
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    Header,
+    Main,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
