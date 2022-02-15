573bdf684579e4a1724ee475def5977eedfc020a
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-15 12:44:37 +0100

    Refactures fetchApi

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,14 @@ export default {
   },
   data() {
     return {
-      api_key: "d6c46c53bff219eb5ef0e2f7c5b73130",
       films: [],
       tvSeries: [],
       placeholder: "Cerca un film o una serie TV",
+      api: {
+        language: "it-IT",
+        baseUri: "https://api.themoviedb.org/3",
+        api_key: "d6c46c53bff219eb5ef0e2f7c5b73130",
+      },
     };
   },
   methods: {
@@ -30,26 +34,20 @@ export default {
         this.tvSeries = [];
         return;
       }
-
       const config = {
         params: {
           query,
-          api_key: this.api_key,
-          language: "it-IT",
+          api_key: this.api.api_key,
+          language: this.api.language,
         },
       };
-
-      axios
-        .get(`https://api.themoviedb.org/3/search/movie`, config)
-        .then((res) => {
-          this.films = res.data.results;
-        });
-
-      axios
-        .get(`https://api.themoviedb.org/3/search/tv`, config)
-        .then((res) => {
-          this.tvSeries = res.data.results;
-        });
+      this.fetchApi("search/movie", config, "films");
+      this.fetchApi("search/tv", config, "tvSeries");
+    },
+    fetchApi(endpoint, config, target) {
+      axios.get(`${this.api.baseUri}/${endpoint}`, config).then((res) => {
+        this[target] = res.data.results;
+      });
     },
   },
 };
